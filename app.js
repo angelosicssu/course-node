3aa03e6d139246b2b0e80ccad0cb6ab3136a69d0
angelosicssu <angelogabrielive@gmail.com>
2025-10-06 15:49:03 -0400

    feat: 1-async-patterns

--- app.js
+++ app.js
@@ -1,19 +1,18 @@
-const http = require('http');
+const { readFile } = require('fs');
 
-const server = http.createServer((req, res) => {
-    if(req.url === '/') {
-        res.end('Home Page');
-    }
-    if(req.url === '/about') {
-        for(let i = 0; i < 1000; i++) {
-            for(let j = 0; j < 1000; j++) {
-                console.log(`${i} ${j}`);
+const getText = (path) => {
+    return new Promise((resolve, reject) => {
+        readFile(path, 'utf-8', (err, data) => {
+            if(err) {
+                reject(err);
+            } else {
+                resolve(data);
             }
-        }
-        res.end('About Page');
-    }
-    res.end('Error Page');
-})
-server.listen(9000, () => {
-    console.log('Server Listening on port 9000...');
-})
+        })
+    })
+}
+
+getText('./content/first.txt')
+    .then(result => console.log(result))
+    .catch(err => console.log(err)).finally()
+    .finally(() => 'Terminou');
